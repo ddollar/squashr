b4830fb3552a2c98f6df9b96a3e010cdb3fec032
David Dollar <ddollar@gmail.com>
2010-05-11 18:52:27 -0400

    build the binary

--- lib/squashr.rb
+++ lib/squashr.rb
@@ -2,4 +2,10 @@ class Squashr
 
   VERSION = "0.0.1"
 
+  MKSQUASHFS_BIN = File.expand_path("../../ext/squashfs3.4/squashfs-tools/mksquashfs", __FILE__)
+
+  def self.squash(source, target, options=[])
+    %x{ #{MKSQUASHFS_BIN} #{source} #{target} #{options.join(' ')} }
+  end
+
 end
