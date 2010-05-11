449d02bbfc99145fe7a3a74239dde16770d8e4c2
David Dollar <ddollar@gmail.com>
2010-05-11 19:04:10 -0400

    raise exceptions on invalid scenarios

--- lib/squashr.rb
+++ lib/squashr.rb
@@ -1,10 +1,18 @@
 class Squashr
 
+  class SourceDirNotFound  < Exception; end
+  class SourceNotDirectory < Exception; end
+  class TargetExists       < Exception; end
+
   VERSION = "0.0.1"
 
   MKSQUASHFS_BIN = File.expand_path("../../ext/squashfs3.4/squashfs-tools/mksquashfs", __FILE__)
 
   def self.squash(source, target, options=[])
+    raise SourceDirNotFound  unless File.exist?(source)
+    raise SourceNotDirectory unless File.directory?(source)
+    raise TargetExists       if     File.exist?(target)
+
     %x{ #{MKSQUASHFS_BIN} #{source} #{target} #{options.join(' ')} }
   end
 
